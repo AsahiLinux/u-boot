cedf2512da45b29e08c8c57820c3b893364c4ce0
Mark Kettenis <kettenis@openbsd.org>
2022-03-01 21:26:48 +0100

    arm: dts: apple: Add Apple M1 Pro/Max/Ultra device trees
    Device trees for the 14" and 16" MacBook Pro and the Mac Studio.
    Taken from the asahi branch of the Asahi Linux github repository.
    
    Signed-off-by: Mark Kettenis <kettenis@openbsd.org>

---
+++ arch/arm/dts/multi-die-cpp.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0+ OR MIT
+/*
+ * C preprocessor macros for t600x multi die support.
+ */
+
+#ifndef __DTS_APPLE_MULTI_DIE_CPP_H
+#define __DTS_APPLE_MULTI_DIE_CPP_H
+
+/* copied include/linux/stringify.h */
+#define __stringify_1(x...)     #x
+#define __stringify(x...)       __stringify_1(x)
+
+#define __concat_1(x, y...)     x ## y
+#define __concat(x, y...)       __concat_1(x, y)
+
+#define DIE_NODE(a) __concat(a, DIE)
+#define DIE_LABEL(a) __stringify(__concat(a, DIE))
+
+#endif /* !__LINUX_STRINGIFY_H */
